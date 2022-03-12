a90990ff54fec4bcf3d3f1b212e06591880f26fc
yhoi <ddmmmktennis@gmail.com>
2022-03-12 15:15:59 +0900

    fix/deploy

--- package/frontend/pages/index.js
+++ package/frontend/pages/index.js
@@ -1,11 +1,9 @@
 import Header from '../components/Header'
-import UploadingImg from '../components/UploadingImg';
 
 export default function Home( ) {
    return (
     <div>
       <Header />
-      <UploadingImg />
     </div>
   )
 }
